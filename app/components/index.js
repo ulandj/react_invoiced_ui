14e43dbdd30b7d9de6bd7d15e7eeb9a05469be47
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2019-12-24 14:49:53 +0600

    Sign up page intro minor fix

--- app/components/index.js
+++ app/components/index.js
@@ -1,5 +1,6 @@
 import Layout from './Layout';
 import Contacts from './Contacts';
 import Sessions from './Sessions';
+import Users from './Users';
 
-export default { Layout, Contacts, Sessions };
+export default { Layout, Contacts, Sessions, Users };
